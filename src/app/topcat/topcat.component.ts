014de22b5203478f61a4933d75fcf57d6c4910d8
helua <helua@users.noreply.github.com>
2023-01-25 00:19:58 +0100

    top cat finetune

--- src/app/topcat/topcat.component.ts
+++ src/app/topcat/topcat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
+import { faBars , faCalendar, faMapPin} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-topcat',
@@ -9,7 +10,8 @@ import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
 export class TopcatComponent implements OnInit {
   title = 'ZWR | TOP CAT ';
   description: MetaDefinition = {name: 'description', content: 'TOP CAT - mixed ultimate tournament by ZWR, 28-30/7/2022 Pułtusk, Poland'};
-
+  cal = faCalendar;
+  pin = faMapPin;
 
   constructor(private titleService: Title, private metaService: Meta) { }
 
