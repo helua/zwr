0857b7c1571ea3ffcb604258e6fafda6d91431f8
Andrzej Helio <ndrzjhl@gmail.com>
2025-02-04 12:40:18 +0000

    fix

--- src/app/feed.service.ts
+++ src/app/feed.service.ts
@@ -17,7 +17,7 @@ export class FeedService {
   constructor( private http: HttpClient ) {}
 
   getProducts(){
-    return this.http.get(`https://887dorwc.api.sanity.io/v2021-10-21/data/query/production?query=*[_type=="product"]{title, priority, slug, defaultProductVariant, tags, "categoryTitles": categories[]->title, "statusTitles": statuses[]->title, "vendor": vendor->title, body, capacity, privateBathroom, address, "bedTypes": bedTypes[]->title, "standard": standard->title} | order(_createdAt desc)`, {
+    return this.http.get(`https://887dorwc.api.sanity.io/v2021-10-21/data/query/production?query=*[_type=="product"]{title, priority, slug, defaultProductVariant, tags, "categoryTitles": categories[]->title, "statusTitles": statuses[]->title, "vendor": vendor->title, body, capacity, privateBathroom, address, "bedTypes": bedTypes[]->title, "standard": standard->title}`, {
       headers: {
         'Content-Type': 'application/json',
       },
